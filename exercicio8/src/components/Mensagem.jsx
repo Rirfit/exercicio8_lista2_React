c01ed14177d85c2d4f8cf30c956f5a5cf8801723
Rirfit <lilrirfit@gmail.com>
2024-10-20 11:17:18 -0300

    FC

---
+++ exercicio8/src/components/Mensagem.jsx
@@ -0,0 +1,4 @@
+function Mensagem(props){
+    let dia = props.dia
+    return (dia ? 'Bom dia': 'Boa noite')
+}export default Mensagem
--- exercicio8/src/main.jsx
+++ exercicio8/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Mensagem from './components/Mensagem'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Mensagem dia ={true}/> <br/>
+    <Mensagem dia ={false}/>
   </StrictMode>,
 )
